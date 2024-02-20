6e36c38b08b7869d493662efdc5161341e2a7830
mrsage-101 <abubakaryourkey309@gmail.com>
2024-02-21 02:51:33 +0500

    Removed all other files except the Assignment2

--- Quiz-1/DOM Forms.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    let x = document.forms['myForm']['fname'].value;
-
-    if (x.trim() === "") {
-        alert("Please fill in the name field.");
-        return false;
-    }
-}
